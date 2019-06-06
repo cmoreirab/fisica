444d84bb0e33a6420d883aea264226d844402e54
cmoreirab <cristobalmorerab@gmail.com>
2019-06-06 12:51:02 -0400

    Se añadio las curvas negativas y posivitas y la combinacion de ambas, pero la combinacion tiene un error y solo se grafica la positiva

--- interface/tarea01.py
+++ interface/tarea01.py
@@ -216,6 +216,178 @@ def conica_de_papus():
     pass
 def Curva_de_Arquitas():
     # añadir sus códigos aca
+    """""
+        Tipo de curva: Curva de Arquitas
+
+      Esta es la animacion de la curva positiva pertenecientes a las curvas de Arquitas.
+
+        Integrantes:
+        Nicolas Fernandez (@mathice)
+        Sebastian Mendez  (@SebaMendez)
+        Cristobal Moreira (@cmoreirab)
+        Gabriel Lara      (@Gabolara453)
+        Dennis Queirolo   (@dennis-queirolo)
+
+        """
+
+    from matplotlib import pyplot as plt
+    import numpy as np
+    import mpl_toolkits.mplot3d.axes3d as p3
+    from matplotlib import animation
+
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
+
+    a = 20
+
+    def gen(n):
+        for theta in np.linspace(-1 / 2 * np.pi, 1 / 2 * np.pi, 100):
+            yield np.array([a * np.cos(theta) ** 2, a * np.cos(theta) * np.sin(theta),
+                            a * (((1 - np.cos(theta)) * np.cos(theta)) ** 1 / 2)])
+
+    def update(num, data, line):
+        line.set_data(data[:2, :num])
+        line.set_3d_properties(data[2, :num])
+
+    N = 1000
+    data = np.array(list(gen(N))).T
+    line, = ax.plot(data[0, 0:1], data[1, 0:1], data[2, 0:1])
+
+    # Setting the axes properties
+    ax.set_xlim3d([-20.0, 20.0])
+    ax.set_xlabel('X')
+
+    ax.set_ylim3d([-20.0, 20.0])
+    ax.set_ylabel('Y')
+
+    ax.set_zlim3d([-20.0, 10.0])
+    ax.set_zlabel('Z')
+
+    ani = animation.FuncAnimation(fig, update, N, fargs=(data, line), interval=10000 / N, blit=False)
+
+    # ani.save('matplot003.gif', writer='imagemagick')
+    plt.show()
+
+    """""
+    Esta es la animacion de la curva negativa pertenecientes a las curvas de Arquitas.
+    """
+
+    from matplotlib import pyplot as plt
+    import numpy as np
+    import mpl_toolkits.mplot3d.axes3d as p3
+    from matplotlib import animation
+
+    fig = plt.figure()
+    ab = p3.Axes3D(fig)
+
+    a = 20
+
+    def gen1(n):
+        for theta in np.linspace(-1 / 2 * np.pi, 1 / 2 * np.pi, 100):
+            yield np.array([a * np.cos(theta) ** 2, a * np.cos(theta) * np.sin(theta),
+                            - a * (((1 - np.cos(theta)) * np.cos(theta)) ** 1 / 2)])
+
+    def update1(num1, data1, line1):
+        line1.set_data(data1[:2, :num1])
+        line1.set_3d_properties(data1[2, :num1])
+
+    N = 1000
+    data1 = np.array(list(gen1(N))).T
+    line1, = ab.plot(data1[0, 0:1], data1[1, 0:1], data1[2, 0:1])
+
+    # Setting the axes properties
+    ab.set_xlim3d([-20.0, 20.0])
+    ab.set_xlabel('X')
+
+    ab.set_ylim3d([-20.0, 20.0])
+    ab.set_ylabel('Y')
+
+    ab.set_zlim3d([-20.0, 10.0])
+    ab.set_zlabel('Z')
+
+    ani = animation.FuncAnimation(fig, update1, N, fargs=(data1, line1), interval=10000 / N, blit=False, repeat=False)
+
+    # ani.save('matplot003.gif', writer='imagemagick')
+    plt.show()
+
+    """""
+    Este es el intento de graficacion en 3D de la curva de Arquitas completa.
+ """
+
+    from matplotlib import pyplot as plt
+    import numpy as np
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib import animation
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ab = fig.gca(projection='3d')
+
+    a = 20
+
+    theta = np.linspace(-1 / 2 * np.pi, 1 / 2 * np.pi, 100)
+    t = 10
+    x1 = a * np.cos(theta) ** 2
+    y1 = a * np.cos(theta) * np.sin(theta)
+    z1 = - a * ((1 - np.cos(theta)) * (np.cos(theta)) ** 1 / 2)
+    z = a * (((1 - np.cos(theta)) * np.cos(theta)) ** 1 / 2)
+    x = a * np.cos(theta) ** 2
+    y = a * np.cos(theta) * np.sin(theta)
+    N = 1000
+
+    def gen(N):
+        for theta in np.linspace(-1 / 2 * np.pi, 1 / 2 * np.pi, 100):
+            x = a * np.cos(theta) ** 2
+            y = a * np.cos(theta) * np.sin(theta)
+            z = a * (((1 - np.cos(theta)) * np.cos(theta)) ** 1 / 2)
+            yield np.array([x, y, z])
+
+    def update(num, data, line):
+        line.set_data(data[:2, :num])
+        line.set_3d_properties(data[2, :num])
+
+    def gen1(N):
+        for theta in np.linspace(-1 / 2 * np.pi, 1 / 2 * np.pi, 100):
+            x1 = a * np.cos(theta) ** 2
+            y1 = a * np.cos(theta) * np.sin(theta)
+            z1 = -a * (((1 - np.cos(theta)) * np.cos(theta)) ** 1 / 2)
+            yield np.array([x1, y1, z1])
+
+    def update1(num1, data1, line1):
+        line1.set_data1(data1[:2, :num1])
+        line1.set_3d_properties(data1[2, :num1])
+
+    data = np.array(list(gen(N))).T
+    data1 = np.array(list(gen1(N))).T
+    line, = ax.plot(data[0, 0:1], data[1, 0:1], data[2, 0:1])
+    line1, = ab.plot(data1[0, 0:1], data1[-1, 0:1], data1[-2, 0:1])
+
+    # Setting the axes properties
+    ax.set_xlim3d([-20.0, 20.0])
+    ax.set_xlabel('X')
+
+    ab.set_xlim3d([20.0, -20.0])
+    ab.set_xlabel('X')
+
+    ax.set_ylim3d([-20.0, 20.0])
+    ax.set_ylabel('Y')
+
+    ab.set_ylim3d([20.0, -20.0])
+    ab.set_ylabel('Y')
+
+    ax.set_zlim3d([-20.0, 10.0])
+    ax.set_zlabel('Z')
+
+    ab.set_zlim3d([20.0, -10.0])
+    ab.set_zlabel('Z')
+
+    ani = animation.FuncAnimation(fig, update, N, fargs=(data, line), interval=10000 / N, blit=False)
+
+    ani1 = animation.FuncAnimation(fig, update1, N, fargs=(data1, line1), interval=10000 / N, blit=False)
+
+    # ani.save('matplot003.gif', writer='imagemagick')
+    plt.show()
+
     pass
 def horoptera():
     # añadir sus códigos aca
